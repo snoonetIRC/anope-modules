6841f9700db7d1100dd6f27006e8b722c030e072
linuxdaemon <linuxdaemon@snoonet.org>
2019-03-14 13:34:20 -0500

    Add travis CI build

---
+++ test_build.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+if [[ -z $ANOPE_VERSION ]]; then
+    ANOPE_VERSION="2.0"
+fi
+
+wget -c -O anope.tar.gz "https://github.com/anope/anope/archive/${ANOPE_VERSION}.tar.gz"
+
+tar xvf anope.tar.gz
+
+SRC_DIR="anope-${ANOPE_VERSION}"
+
+cp -r modules/* $SRC_DIR/modules/third/
+
+cd $SRC_DIR
+
+cat > config.cache << EOF
+INSTDIR="$(pwd)/run"
+RUNGROUP=""
+UMASK=077
+DEBUG="yes"
+USE_PCH="no"
+EXTRA_INCLUDE_DIRS=""
+EXTRA_LIB_DIRS=""
+EXTRA_CONFIG_ARGS=""
+EOF
+
+./Config -quick -nointro && pushd build && make
+
