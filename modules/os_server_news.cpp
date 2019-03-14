033af0dd1e1c10a96c66524b542cae1e6ec77852
linuxdaemon <linuxdaemon@snoonet.org>
2019-03-14 12:59:06 -0500

    Reformat code according to codestyle

--- modules/os_server_news.cpp
+++ modules/os_server_news.cpp
@@ -3,35 +3,44 @@
 
 #include "module.h"
 
-struct ServerNewsItem : Serializable
+struct ServerNewsItem
+	: Serializable
 {
 	Anope::string text;
 	Anope::string who;
 	Anope::string server;
 	time_t time;
 
-	ServerNewsItem() : Serializable("ServerNewsItem") { }
+	ServerNewsItem()
+		: Serializable("ServerNewsItem")
+	{
+	}
 };
 
-class ServerNewsService : public Service
+class ServerNewsService
+	: public Service
 {
  public:
-	ServerNewsService(Module *m) : Service(m, "ServerNewsService", "server_news") { }
+	ServerNewsService(Module* m)
+		: Service(m, "ServerNewsService", "server_news")
+	{
+	}
 
-	virtual ServerNewsItem *CreateNewsItem() = 0;
+	virtual ServerNewsItem* CreateNewsItem() = 0;
 
-	virtual void AddNewsItem(ServerNewsItem *n) = 0;
+	virtual void AddNewsItem(ServerNewsItem* n) = 0;
 
-	virtual void DelNewsItem(ServerNewsItem *n) = 0;
+	virtual void DelNewsItem(ServerNewsItem* n) = 0;
 
-	virtual std::vector<ServerNewsItem *> &GetNewsList() = 0;
+	virtual std::vector<ServerNewsItem*>& GetNewsList() = 0;
 };
 
 static ServiceReference<ServerNewsService> news_service("ServerNewsService", "server_news");
 
-struct MyNewsItem : ServerNewsItem
+struct MyNewsItem
+	: ServerNewsItem
 {
-	void Serialize(Serialize::Data &data) const anope_override
+	void Serialize(Serialize::Data& data) const anope_override
 	{
 		data["text"] << this->text;
 		data["who"] << this->who;
@@ -39,14 +48,14 @@ struct MyNewsItem : ServerNewsItem
 		data["server"] << this->server;
 	}
 
-	static Serializable* Unserialize(Serializable *obj, Serialize::Data &data)
+	static Serializable* Unserialize(Serializable* obj, Serialize::Data& data)
 	{
 		if (!news_service)
 			return NULL;
 
-		ServerNewsItem *ni;
+		ServerNewsItem* ni;
 		if (obj)
-			ni = anope_dynamic_static_cast<ServerNewsItem *>(obj);
+			ni = anope_dynamic_static_cast<ServerNewsItem*>(obj);
 		else
 			ni = news_service->CreateNewsItem();
 
@@ -61,13 +70,17 @@ struct MyNewsItem : ServerNewsItem
 	}
 };
 
-typedef std::vector<ServerNewsItem *> NewsItems;
+typedef std::vector<ServerNewsItem*> NewsItems;
 
-class MyNewsService : public ServerNewsService
+class MyNewsService
+	: public ServerNewsService
 {
 	NewsItems newsItems;
  public:
-	MyNewsService(Module *m) : ServerNewsService(m) { }
+	MyNewsService(Module* m)
+		: ServerNewsService(m)
+	{
+	}
 
 	~MyNewsService()
 	{
@@ -75,27 +88,27 @@ class MyNewsService : public ServerNewsService
 			delete newsItems[j];
 	}
 
-	ServerNewsItem *CreateNewsItem() anope_override
+	ServerNewsItem* CreateNewsItem() anope_override
 	{
 		return new MyNewsItem();
 	}
 
-	void AddNewsItem(ServerNewsItem *n)
+	void AddNewsItem(ServerNewsItem* n)
 	{
 		this->newsItems.push_back(n);
 		n->QueueUpdate();
 	}
 
-	void DelNewsItem(ServerNewsItem *n)
+	void DelNewsItem(ServerNewsItem* n)
 	{
-		NewsItems &list = this->GetNewsList();
+		NewsItems& list = this->GetNewsList();
 		NewsItems::iterator it = std::find(list.begin(), list.end(), n);
 		if (it != list.end())
 			list.erase(it);
 		delete n;
 	}
 
-	NewsItems &GetNewsList()
+	NewsItems& GetNewsList()
 	{
 		return this->newsItems;
 	}
@@ -114,20 +127,22 @@ class MyNewsService : public ServerNewsService
 	}
 };
 
-class CommandOSServerNews : public Command
+class CommandOSServerNews
+	: public Command
 {
 	ServiceReference<ServerNewsService> ns;
 
  protected:
-	void DoList(CommandSource &source)
+	void DoList(CommandSource& source)
 	{
-		NewsItems &list = this->ns->GetNewsList();
+		NewsItems& list = this->ns->GetNewsList();
 		if (list.empty())
 			source.Reply(_("There is no server news."));
 		else
 		{
 			ListFormatter lflist(source.GetAccount());
-			lflist.AddColumn(_("Number")).AddColumn(_("Creator")).AddColumn(_("Created")).AddColumn(_("Server")).AddColumn(_("Text"));
+			lflist.AddColumn(_("Number")).AddColumn(_("Creator")).AddColumn(_("Created")).AddColumn(
+				_("Server")).AddColumn(_("Text"));
 
 			for (unsigned i = 0, end = list.size(); i < end; ++i)
 			{
@@ -152,7 +167,7 @@ class CommandOSServerNews : public Command
 		}
 	}
 
-	void DoAdd(CommandSource &source, const std::vector<Anope::string> &params)
+	void DoAdd(CommandSource& source, const std::vector<Anope::string>& params)
 	{
 		Anope::string text, server;
 		server = params.size() > 1 ? params[1] : "";
@@ -165,7 +180,7 @@ class CommandOSServerNews : public Command
 			if (Anope::ReadOnly)
 				source.Reply(READ_ONLY_MODE);
 
-			ServerNewsItem *news = this->ns->CreateNewsItem();
+			ServerNewsItem* news = this->ns->CreateNewsItem();
 			news->text = text;
 			news->time = Anope::CurTime;
 			news->who = source.GetNick();
@@ -178,15 +193,15 @@ class CommandOSServerNews : public Command
 		}
 	}
 
-	void DoDel(CommandSource &source, const std::vector<Anope::string> &params)
+	void DoDel(CommandSource& source, const std::vector<Anope::string>& params)
 	{
-		const Anope::string &text = params.size() > 1 ? params[1] : "";
+		const Anope::string& text = params.size() > 1 ? params[1] : "";
 
 		if (text.empty())
 			this->OnSyntaxError(source, "DEL");
 		else
 		{
-			NewsItems &list = this->ns->GetNewsList();
+			NewsItems& list = this->ns->GetNewsList();
 			if (list.empty())
 				source.Reply(_("There is no server news."));
 			else
@@ -206,7 +221,9 @@ class CommandOSServerNews : public Command
 							return;
 						}
 					}
-					catch (const ConvertException &) { }
+					catch (const ConvertException&)
+					{
+					}
 
 					source.Reply(_("Server news item #%s not found!"), text.c_str());
 				}
@@ -221,12 +238,12 @@ class CommandOSServerNews : public Command
 		}
 	}
 
-	void DoNews(CommandSource &source, const std::vector<Anope::string> &params)
+	void DoNews(CommandSource& source, const std::vector<Anope::string>& params)
 	{
 		if (!this->ns)
 			return;
 
-		const Anope::string &cmd = params[0];
+		const Anope::string& cmd = params[0];
 
 		if (cmd.equals_ci("LIST"))
 			return this->DoList(source);
@@ -237,8 +254,11 @@ class CommandOSServerNews : public Command
 		else
 			this->OnSyntaxError(source, "");
 	}
+
  public:
-	CommandOSServerNews(Module *creator) : Command(creator, "operserv/servernews", 1, 3), ns(news_service)
+	CommandOSServerNews(Module* creator)
+		: Command(creator, "operserv/servernews", 1, 3)
+		, ns(news_service)
 	{
 		this->SetDesc(_("Define messages to be shown to users on specific servers at logon"));
 
@@ -247,12 +267,12 @@ class CommandOSServerNews : public Command
 		this->SetSyntax("LIST");
 	}
 
-	void Execute(CommandSource &source, const std::vector<Anope::string> &params) anope_override
+	void Execute(CommandSource& source, const std::vector<Anope::string>& params) anope_override
 	{
 		return this->DoNews(source, params);
 	}
 
-	bool OnHelp(CommandSource &source, const Anope::string &subcommand) anope_override
+	bool OnHelp(CommandSource& source, const Anope::string& subcommand) anope_override
 	{
 		this->SendSyntax(source);
 		source.Reply(" ");
@@ -263,7 +283,8 @@ class CommandOSServerNews : public Command
 	}
 };
 
-class OSNews : public Module
+class OSNews
+	: public Module
 {
 	MyNewsService newsservice;
 	Serialize::Type newsitem_type;
@@ -272,13 +293,13 @@ class OSNews : public Module
 
 	Anope::string announcer;
 
-	void DisplayNews(User *u)
+	void DisplayNews(User* u)
 	{
 		NewsItems newsList = this->newsservice.GetNewsItemsForServer(u->server);
 		if (newsList.empty())
 			return;
 
-		BotInfo *bi = BotInfo::Find(announcer, true);
+		BotInfo* bi = BotInfo::Find(announcer, true);
 		if (bi == NULL)
 			return;
 
@@ -286,25 +307,28 @@ class OSNews : public Module
 
 		for (unsigned i = 0, end = newsList.size(); i < end; ++i)
 		{
-			u->SendMessage(bi, msg.c_str(), Anope::strftime(newsList[i]->time, u->Account(), true).c_str(), newsList[i]->text.c_str());
+			u->SendMessage(bi, msg.c_str(), Anope::strftime(newsList[i]->time, u->Account(), true).c_str(),
+						   newsList[i]->text.c_str());
 		}
 	}
 
  public:
-	OSNews(const Anope::string &modname, const Anope::string &creator) : Module(modname, creator, THIRD),
-		newsservice(this), newsitem_type("ServerNewsItem", MyNewsItem::Unserialize),
-		commandOSServerNews(this)
+	OSNews(const Anope::string& modname, const Anope::string& creator)
+		: Module(modname, creator, THIRD)
+		, newsservice(this)
+		, newsitem_type("ServerNewsItem", MyNewsItem::Unserialize)
+		, commandOSServerNews(this)
 	{
 		this->SetAuthor("linuxdaemon");
 		this->SetVersion("0.1");
 	}
 
-	void OnReload(Configuration::Conf *conf) anope_override
+	void OnReload(Configuration::Conf* conf) anope_override
 	{
 		announcer = conf->GetModule(this)->Get<const Anope::string>("announcer", "Global");
 	}
 
-	void OnUserConnect(User *user, bool &) anope_override
+	void OnUserConnect(User* user, bool&) anope_override
 	{
 		if (user->Quitting() || !user->server->IsSynced())
 			return;
