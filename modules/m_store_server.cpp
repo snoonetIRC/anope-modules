37016e1b85d432fd591c17a0454591a0d33dfe3e
linuxdaemon <linuxdaemon@snoonet.org>
2019-04-23 20:31:41 -0500

    Update m_store_server
    - Make regserver set logic more central
    - Set regserver to "" when no server is detected
    - Add missing override directives

--- modules/m_store_server.cpp
+++ modules/m_store_server.cpp
@@ -3,38 +3,40 @@
 class ModuleStoreServer
 	: public Module
 {
+	void SetServer(const Serialize::Reference<NickCore>& nc, User* user)
+	{
+		if (!nc)
+			return;
+
+		Anope::string* ext = serverExt.Get(nc);
+		if (ext && !ext->empty())
+			return;
+
+		serverExt.Set(nc, user && user->server ? user->server->GetName() : "");
+	}
+
 	SerializableExtensibleItem<Anope::string> serverExt;
+
  public:
 	ModuleStoreServer(const Anope::string& modname, const Anope::string& creator)
 		: Module(modname, creator, THIRD)
 		, serverExt(this, "REGSERVER")
 	{
 		this->SetAuthor("linuxdaemon");
-		this->SetVersion("0.1");
+		this->SetVersion("0.2");
 	}
 
-	virtual void OnNickRegister(User* user, NickAlias* na, const Anope::string& pass)
+	void OnNickRegister(User* user, NickAlias* na, const Anope::string&) anope_override
 	{
-		if (!na || !na->nc)
+		if (!na)
 			return;
 
-		serverExt.Set(na->nc, user ? user->server->GetName() : "");
-		na->QueueUpdate();
-		na->nc->QueueUpdate();
+		SetServer(na->nc, user);
 	}
 
-	virtual void OnPostCommand(CommandSource& source, Command* command, const std::vector<Anope::string>& params)
+	void OnPostCommand(CommandSource& source, Command*, const std::vector<Anope::string>&)  anope_override
 	{
-		NickCore* nc = source.GetAccount();
-		if (!nc || serverExt.HasExt(nc))
-			return;
-
-		User* u = source.GetUser();
-		if (!u || !u->server)
-			return;
-
-		serverExt.Set(nc, u->server->GetName());
-		nc->QueueUpdate();
+		SetServer(source.GetAccount(), source.GetUser());
 	}
 };
 
